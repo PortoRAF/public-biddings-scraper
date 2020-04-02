77496a1e4ece2dc3f2f085af349a95ebf0922ecf
Renato Porto <renatoafporto@gmail.com>
2020-04-02 01:26:26 -0300

    Made async call to search params

--- app.js
+++ app.js
@@ -3,32 +3,25 @@ const cheerio = require("cheerio");
 const fillArray = require("./utils/fillArray");
 
 const url = "http://www.novalima.mg.gov.br/portal-transparencia/editais";
+const buscas = ["edital concorrencia", "edital tomada"];
 var editais = [];
 
-axios
-  .get(url, {
-    params: {
-      busca: "edital concorrencia"
-    }
-  })
-  .then(response => {
-    editais = editais.concat(fillArray(response.data));
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const webScraping = async () => {
+  for (const busca of buscas) {
+    await axios
+      .get(url, {
+        params: {
+          busca: busca
+        }
+      })
+      .then(response => {
+        editais = editais.concat(fillArray(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  console.log(editais);
+};
 
-axios
-  .get(url, {
-    params: {
-      busca: "edital tomada"
-    }
-  })
-  .then(response => {
-    editais = editais.concat(fillArray(response.data));
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-// console.log(editais);
+webScraping();
