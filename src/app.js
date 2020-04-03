eb798ec004b28e25a1fd276b09858de2d1999e5e
Renato Porto <renatoafporto@gmail.com>
2020-04-03 19:23:47 -0300

    Set timezone to cron job

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const cron = require("node-cron");
 const express = require("express");
-const path = require("path");
 const webScraping = require("./utils/webScraping");
 const sendNotification = require("./emails/sendNotificaton");
 
@@ -8,22 +7,27 @@ const url = "http://www.novalima.mg.gov.br/portal-transparencia/editais";
 const buscas = ["edital concorrencia", "edital tomada"];
 var editais = [];
 
-const mailToList = [
-  "renatoafporto@gmail.com"
-];
+const mailToList = ["renatoafporto@gmail.com"];
 
 app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.send("Server is up!");
 });
 
-cron.schedule("0 19 * * 1-5", async () => {
-  editais = await webScraping(url, buscas, editais);
-  if (editais.length > 0) {
-    sendNotification(editais, mailToList.toString());
-    // console.log(editais);
+cron.schedule(
+  "0 * * * 1-5",
+  async () => {
+    editais = await webScraping(url, buscas, editais);
+    if (editais.length > 0) {
+      sendNotification(editais, mailToList.toString());
+      // console.log(editais);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "America/Sao_Paulo",
   }
-});
+);
 
 app.listen(process.env.PORT);
