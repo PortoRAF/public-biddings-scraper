75bc17acd3ccc2d183842e89530adba0f2d6bf35
Renato Porto <renatoafporto@gmail.com>
2020-04-03 20:44:43 -0300

    Configured expressJS to use static pages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const cron = require("node-cron");
 const express = require("express");
+const path = require("path");
 const webScraping = require("./utils/webScraping");
 const sendNotification = require("./emails/sendNotificaton");
 
 const url = "http://www.novalima.mg.gov.br/portal-transparencia/editais";
 const buscas = ["edital concorrencia", "edital tomada"];
+
 var editais = [];
 
 const mailToList = ["renatoafporto@gmail.com"];
 
+const publicDirectoryPath = path.join(__dirname, "../public");
+
 app = express();
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.send("Server is up!");
