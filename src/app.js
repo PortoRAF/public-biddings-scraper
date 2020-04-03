d8217be618819db5a86b8b8618b0f335fd135a56
Renato Porto <renatoafporto@gmail.com>
2020-04-03 16:28:30 -0300

    Set test notificatino

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Server is up!");
 });
 
-cron.schedule("30 7 * * 1-5", async () => {
+cron.schedule("0 19 * * 1-5", async () => {
   editais = await webScraping(url, buscas, editais);
   if (editais.length > 0) {
     sendNotification(editais, mailToList.toString());
